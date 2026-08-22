7a2145a5dc255f1211975103581d86ca2ee01e35
Omar <omarkmu@gmail.com>
2026-08-22 14:53:29 -0400

    Refactor command rolling logic
    Extracted rolling logic to a function and made it so that '1dN' rolls
    display as 'dN' instead.

--- Contents/mods/roll-command-plus/common/media/lua/client/roll-command-plus-Command.lua
+++ Contents/mods/roll-command-plus/common/media/lua/client/roll-command-plus-Command.lua
@@ -1,70 +1,71 @@
 local dice = require("roll-command-plus-Roll")
 
-local _SendCommandToServer = SendCommandToServer
-function _G.SendCommandToServer(command)
+local function doRolls(command)
+    local grandTotal = 0
+    local grandResults = {}
+    local grandRolls = {}
+    for die in string.gmatch(command, "([^ ]+)") do
+        local n, s = die:match("([^,]+)d([^,]+)")
 
-    local rollTextBase = "/roll"
-    local startsWithRoll = command:sub(1, #rollTextBase) == rollTextBase
-    if startsWithRoll then
+        if not n and not s and die ~= '+' then
+            n = 1
+            s = tonumber(die) or string.gsub(die, "d", "")
+        end
 
-        local rollCommand = "/roll "
+        if n and s then
+            if n == 1 then
+                grandRolls[#grandRolls + 1] = "d"..s
+            else
+                grandRolls[#grandRolls + 1] = n.."d"..s
+            end
 
-        if string.find(command, "/rollall ") then rollCommand = "/rollall " end
-        if string.find(command, "/rollyell ") then rollCommand = "/rollyell " end
-
-        local grandTotal = 0
-        local grandResults
-        local rolling
-        local dieCount = 0
-
-        command = command:gsub(rollCommand, "")
-        command = command:gsub('^%s*(.-)%s*$', '%1')
-
-        if command == "" or command==rollTextBase then command = "1d6" end
-
-        for die in string.gmatch(command, "([^ ]+)") do
-            if die ~= rollCommand then
-
-                local n, s = die:match("([^,]+)d([^,]+)")
-
-                if not n and not s and die ~= '+' then
-                    n = 1
-                    s = tonumber(die) or string.gsub(die, "d", "")
+            local total, results = dice.roll(tonumber(n),tonumber(s))
+            if total and results and type(results)=="table" then
+                for i = 1, #results do
+                    grandResults[#grandResults + 1] = results[i] 
                 end
-
-                if n and s then
-                    dieCount = dieCount+n
-                    rolling = (rolling and rolling..", " or "") .. n.."d"..s
-
-                    local total, results = dice.roll(tonumber(n),tonumber(s))
-                    if total and results and type(results)=="table" then
-                        for k,result in pairs(results) do
-                            grandResults = (grandResults and grandResults.." + " or "") .. result
-                        end
-                        grandTotal = grandTotal + total
-                    end
-                end
+                grandTotal = grandTotal + total
             end
         end
-        
-        if not grandResults then return end
-        local printOut = "Rolling: "..rolling..", Results: "..grandResults
-        if dieCount > 1 then printOut = printOut.." = ("..grandTotal..")" end
+    end
 
-        --roleplaychat patch
-        local rpChat = getActivatedMods():contains("roleplaychat")
-        if rpChat then printOut = "["..getPlayer():getDescriptor():getForename().."]: "..printOut end
+    return grandTotal, grandResults, grandRolls
+end
 
-        if rollCommand == "/rollall " then
-            processGeneralMessage(printOut)
-        elseif rollCommand == "/rollyell " then
-            processShoutMessage(printOut)
-        else
-            processSayMessage(printOut)
-        end
-
+local _SendCommandToServer = SendCommandToServer
+function _G.SendCommandToServer(command)
+    local rollTextBase = "/roll"
+    local startsWithRoll = command:sub(1, #rollTextBase) == rollTextBase
+    if not startsWithRoll then
+        _SendCommandToServer(command)
         return
     end
 
-    _SendCommandToServer(command)
+    local rollCommand = "/roll "
+
+    if string.find(command, "/rollall ") then rollCommand = "/rollall " end
+    if string.find(command, "/rollyell ") then rollCommand = "/rollyell " end
+
+    command = command:gsub(rollCommand, "")
+    command = command:gsub('^%s*(.-)%s*$', '%1')
+
+    if command == "" or command==rollTextBase then command = "1d6" end
+    local total, results, rolls = doRolls(command)
+
+    if #results == 0 then return end
+    
+    local printOut = "Rolling: "..table.concat(rolls, ', ')..", Results: "..table.concat(results, ' + ')
+    if #results > 1 then printOut = printOut.." = ("..total..")" end
+
+    --roleplaychat patch
+    local rpChat = getActivatedMods():contains("roleplaychat")
+    if rpChat then printOut = "["..getPlayer():getDescriptor():getForename().."]: "..printOut end
+
+    if rollCommand == "/rollall " then
+        processGeneralMessage(printOut)
+    elseif rollCommand == "/rollyell " then
+        processShoutMessage(printOut)
+    else
+        processSayMessage(printOut)
+    end
 end
